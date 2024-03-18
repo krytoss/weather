a2fd5f392181750fbce10a49d39e148c07532018
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-18 14:30:25 +0100

    Added an option to get a location on click

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,7 +1,38 @@
+import { useState } from "react"
+
 const WeatherApp = () => {
+
+    const [location, setLocation] = useState(null)
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                locationSuccess,
+                locationError,
+                {
+                    enableHighAccuracy: false,
+                    maximumAge: Infinity
+                }
+            )
+        } else {
+            alert("Geolocation not supported")
+        }
+    }
+
+    const locationSuccess = (pos) => {
+        const lat = pos.coords.latitude
+        const lon = pos.coords.longitude
+        setLocation({ lat: lat, lon: lon })
+    }
+
+    const locationError = () => {
+        alert("Unable to retrieve your location.")
+    }
+
     return (
         <div id='weather'>
-
+            { !location && <p>Getting a location...</p> }
+            <button onClick={ getLocation }>Get loc</button>
         </div>
     )
 }
